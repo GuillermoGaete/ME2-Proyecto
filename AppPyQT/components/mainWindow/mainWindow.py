edea9f3645bf0778c9b0d1ccaeec42f0b041dbcd
guillermo <you@example.com>
2019-10-05 11:46:54 -0300

    Added new files

--- AppPyQT/components/mainWindow/mainWindow.py
+++ AppPyQT/components/mainWindow/mainWindow.py
@@ -1,8 +1,21 @@
+import serial
+import serial.tools.list_ports as list_ports
+
 from mainWindow_ui import *
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        self.onLoadWindow()
+        
+    def onLoadWindow(self):
+        portList=list_ports.comports()
+        for port in portList:
+            mainPort = self.menuPrincipal.menuDispositivos.menuArduino.menuPort
+            print(port.device)
+            print(mainMenu)
+            mainMenu.addMenu('File')
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
